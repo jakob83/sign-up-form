0410db3b2bd008d61ec98a0af79159f203429853
jakob83 <jakobpllo@gmail.com>
2024-02-16 10:52:44 +0100

    siu

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const password = document.getElementById("password");
+const password2 = document.getElementById("rp-password");
+const form = document.getElementById("form");
 
+
+form.addEventListener("submit", function (event) {
+    if (password.value != password2.value) {
+        console.log(false);
+        event.preventDefault();
+    }
+})
